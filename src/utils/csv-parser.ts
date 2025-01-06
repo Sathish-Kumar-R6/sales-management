93e56021f7e999d567da97a0f58e15bd2c364595
SATHISH-ASUS\sathi <sathish662001@gmail.com>
2025-01-06 23:12:16 +0530

    fix(csv parser): throw error if file not valid

--- src/utils/csv-parser.ts
+++ src/utils/csv-parser.ts
@@ -4,11 +4,28 @@ import { SalesCsvType } from "../services/sales.types";
 
 export async function parseCSV(filePath: string): Promise<SalesCsvType[]> {
   const results: SalesCsvType[] = [];
+
   return new Promise((resolve, reject) => {
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", () => resolve(results))
-      .on("error", (err) => reject(err));
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        return reject(new Error("File not found or inaccessible"));
+      }
+
+      const readStream = fs.createReadStream(filePath);
+      readStream
+        .on("error", () => {
+          reject(
+            new Error("Failed to read the file. Ensure it is a valid CSV file"),
+          );
+        })
+        .pipe(csv())
+        .on("data", (data) => {
+          results.push(data);
+        })
+        .on("end", () => resolve(results))
+        .on("error", () =>
+          reject(new Error("An error occurred while parsing the CSV file")),
+        );
+    });
   });
 }
